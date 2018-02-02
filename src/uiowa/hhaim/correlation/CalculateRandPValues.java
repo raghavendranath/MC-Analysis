2209fc13aa0f608f9797270f194f22da959ebd62
raghavendranath <raghavendranath.kandula525@gmail.com>
2018-02-02 13:45:28 -0600

    Modified options associated with CalculateRandPvalues

--- src/uiowa/hhaim/correlation/CalculateRandPValues.java
+++ src/uiowa/hhaim/correlation/CalculateRandPValues.java
@@ -24,7 +24,7 @@ class Excel {
 }
 public class CalculateRandPValues {
 
-    private static final String Datafile = "U:\\ResearchData\\rdss_hhaim\\LAB PROJECTS\\Raghav\\Analysis\\New Project\\AllClades_JustSequences\\Glyco Sites_All Clades\\Results\\TSV Files\\B_NA_Chronic.txt";
+    private static final String Datafile = "U:\\ResearchData\\rdss_hhaim\\LAB PROJECTS\\Raghav\\Analysis\\New Project\\AllClades_JustSequences\\Glyco Sites_All Clades\\Results\\TSV Files\\testing.txt";
     public static void main(String args[]){
         BufferedReader br = null;
         FileReader fr = null;
@@ -70,9 +70,9 @@ public class CalculateRandPValues {
 
             ArrayList<String> xList = null, yList = null;
             //For ChiSquare Test
-            ChiSquareTest testStatistic = new ChiSquareTest();
+            //ChiSquareTest testStatistic = new ChiSquareTest();
             PrintWriter writer =  new PrintWriter("U:\\ResearchData\\rdss_hhaim\\LAB PROJECTS\\Raghav\\Analysis\\New Project\\AllClades_JustSequences\\Glyco Sites_All Clades\\Results\\output2.txt");
-            for(int i=8; i<13;i++){
+            for(int i=1001; i<1007;i++){
                 xList = excel.excelColumns.get(Integer.toString( i ));
                 xArray = new double[xList.size()];
                 for(int k=0; k< xList.size();k++) {
@@ -80,16 +80,16 @@ public class CalculateRandPValues {
                 }
 
                 //For chi-square test
-                long xDummy[] = new long[xArray.length];
+               /* long xDummy[] = new long[xArray.length];
                 for(int k=0; k< xArray.length;k++) {
-                    if(xArray[k] == 0){
-                        xDummy[k] = 0;//(long)xArray[k];
+                    if(xArray[k] != 0){
+                        xDummy[k] = 1;//(long)xArray[k];
                     }
                     else{
                         xDummy[k] = 1;
                     }
-                }
-                for(int j=i+1; j<13;j++){
+                }*/
+                for(int j=i+1; j<1007;j++){
                     //System.out.print("("+i+","+j+") ");
                     writer.append("("+i+";"+j+"),");
                     yList = excel.excelColumns.get(Integer.toString( j ));
@@ -99,7 +99,7 @@ public class CalculateRandPValues {
                         yArray[k] = Double.parseDouble( yList.get(k) );
                     }
                     //for chi-square test
-                    double yDummy[] = new double[yArray.length];
+                    /*double yDummy[] = new double[yArray.length];
                     for(int k=0; k< yArray.length;k++) {
                         if(yArray[k] == 0){
                             yDummy[k] = 0;//yArray[k];
@@ -107,7 +107,7 @@ public class CalculateRandPValues {
                         else{
                             yDummy[k] = 1;
                         }
-                    }
+                    }*/
 
                     double nonZeroChange = 0.0;
                     double allChanges = 0.0;
@@ -118,17 +118,22 @@ public class CalculateRandPValues {
                             if(yArray[l] !=0 ){
                                 allChanges++;
                                 nonZeroChange++;
+                                continue;
                             }
                             else{
                                 allChanges++;
+                                continue;
                             }
                         }
                         else{
-                            if(yArray[1] != 0 ){
+                            if(yArray[l] != 0 ){
                                      allChanges++;
+                                     continue;
                              }
                         }
                     }
+
+                    System.out.println("("+i+","+j+")   :"+allChanges+","+nonZeroChange);
 
                     double corr = Spearman.getCorrelation( xArray,yArray );
                     //System.out.print(corr+" "+Spearman.getPvalue(corr, xArray.length));
@@ -142,19 +147,26 @@ public class CalculateRandPValues {
                     double noOfZeroOneAndOneZero = allChanges - nonZeroChange;
                     double all = xArray.length;
                     //Option #1
-                    writer.append( ","+ ((noOfOneOne/noOfZeroOneAndOneZero)*(1-(noOfZeroZero/all)) )+"");
+                    writer.append( ","+ ((noOfOneOne/(noOfZeroOneAndOneZero+0.05))*(1-(noOfZeroZero/all)) )+"");
 
                     //option #2
-                    double chiSquare = testStatistic.chiSquare(yDummy, xDummy);
-                    double chiSquareP = testStatistic.chiSquareTest( yDummy, xDummy );
+                    /*double chiSquare = testStatistic.chiSquare(yDummy, xDummy);
+                    double chiSquareP = testStatistic.chiSquareTest( yDummy, xDummy );*/
+                    double chi[] = eliminateZeroZero_ChiSquare( xArray, yArray, allChanges);
+                    if(chi == null){
+                        System.out.println("Bad request!");
+                        System.exit( 0 );
+                    }
+                    double chiSquare = chi[0];
+                    double chiSquareP = chi[1];
                     //Option #2a
-                    writer.append(","+ (chiSquare*(noOfOneOne/all))+"");
+                    /*writer.append(","+ (chiSquare*(noOfOneOne/all))+"");*/
                     //Option #2b
-                    writer.append(","+ (chiSquareP*(noOfOneOne/all))+"");
+                    writer.append(","+ ((1/chiSquareP)*(noOfOneOne/all))+"");
 
                     //Option #3
-                    double rs = eliminateZeroZero_SpearmanCorrelation(xArray,yArray,allChanges);
-                    writer.append(","+ (rs*(1-(noOfZeroZero/all)))+" ");
+                    /*double Prs = eliminateZeroZero_SpearmanCorrelationPValue(xArray,yArray,allChanges);
+                    writer.append(","+ (1/Prs)*(1-(noOfZeroZero/all))+" ");*/
 
 
                     //System.out.println();
@@ -187,7 +199,7 @@ public class CalculateRandPValues {
         }
     }
 
-    public static double eliminateZeroZero_SpearmanCorrelation(double[] x, double[] y, double allChanges){
+    public static double eliminateZeroZero_SpearmanCorrelationPValue(double[] x, double[] y, double allChanges){
         double[] xArray = new double[(int)allChanges];
         double[] yArray = new double[(int)allChanges];
         for(int i=0,k=0; i<x.length && k<(int)allChanges ;i++){
@@ -204,7 +216,100 @@ public class CalculateRandPValues {
                 }
             }
         }
-        return Spearman.getCorrelation( xArray,yArray );
+        /*System.out.println("++++++++++++++++++++++");
+        System.out.println(allChanges);
+
+        for(int i=0; i<xArray.length;i++){
+            System.out.println(xArray[i]+","+yArray[i]);
+        }*/
+        double corr = Spearman.getCorrelation( xArray,yArray );
+        return Spearman.getPvalue(corr, xArray.length);
 
     }
+
+    public static double[] eliminateZeroZero_ChiSquare(double[] x, double[] y, double allChanges) {
+        long[] xArray = new long[(int) allChanges];
+        long[] yArray = new long[(int) allChanges];
+        for (int i = 0, k = 0; i < x.length && k < (int) allChanges; i++) {
+            if (x[i] != 0) {
+                xArray[k] = 1;
+                if (y[i] != 0) {
+                    yArray[k] = 1;
+                } else {
+                    yArray[k] = 0;
+                }
+                k++;
+            } else {
+                if (y[i] != 0) {
+                    xArray[k] = 0;
+                    yArray[k] = 1;
+                    k++;
+                }
+            }
+
+        }
+
+
+        /*for(int i=0; i<xArray.length;i++){
+            System.out.println(xArray[i]+","+yArray[i]);
+        }*/
+        long[][] counts = {xArray, yArray};
+       /* for(int i=0; i<counts.length;i++){
+            for(int j=0; j<counts[i].length;j++){
+                System.out.print(counts[i][j]+",");
+            }
+            System.out.println();
+        }
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");*/
+        //For ChiSquare Test
+        ChiSquareTest testStatistic = new ChiSquareTest();
+        double[] result = new double[2];
+        result[0] = testStatistic.chiSquare(counts );
+        result[1] = testStatistic.chiSquareTest( counts );
+
+
+        //This is for testing
+        /*long[] observed1 = {10, 12, 12, 10, 15};
+        long[] observed2 = {15, 10, 10, 15, 5};
+        long[][] count1 = {observed1, observed2};
+        System.out.println(testStatistic.chiSquare( count1 ));
+        System.out.println(testStatistic.chiSquareTest( count1 ));*/
+        return result;
+    }
+
+
+   //ChiSquare without removing the (0,0) ones
+   /*public static double[] eliminateZeroZero_ChiSquare(double[] x, double[] y){
+       long[] xArray = new long[x.length];
+       long[] yArray = new long[y.length];
+       if(x.length == y.length){
+           for(int i=0; i<x.length;i++){
+               if(x[i] != 0){
+                   xArray[i] = 1;
+               }
+               else{
+                   xArray[i] = 0;
+               }
+           }
+
+           for(int i=0; i<y.length;i++){
+               if(y[i] != 0){
+                   yArray[i] = 1;
+               }
+               else{
+                   yArray[i] = 0;
+               }
+           }
+           //Copying both arrays to counts
+           long[][] counts = {xArray, yArray};
+           ChiSquareTest testStatistic = new ChiSquareTest();
+           double[] result = new double[2];
+           result[0] = testStatistic.chiSquare(counts );
+           result[1] = testStatistic.chiSquareTest( counts );
+           return result;
+
+       }
+       return null;
+   }*/
+
 }
