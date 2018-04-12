97afb90a82327dd585f645bb20f2a3da75ef3884
raghavendranath <raghavendranath.kandula525@gmail.com>
2018-04-12 12:46:21 -0500

    Clade Specific average gp pairs is complete

--- src/uiowa/hhaim/GeneticDistances/CladeSpecificAvgGDPairs.java
+++ src/uiowa/hhaim/GeneticDistances/CladeSpecificAvgGDPairs.java
@@ -2,6 +2,7 @@ package uiowa.hhaim.GeneticDistances;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -11,7 +12,7 @@ import java.util.HashSet;
 public class CladeSpecificAvgGDPairs {
     public static void main(String args[]) {
         if (args.length != 2){
-            System.out.println("Give [Genetic distances computed file location] in the arguments and output file location");
+            System.out.println("Give [Genetic distances computed file location] in the arguments and output file location(without a .txt extension)");
             System.exit(0);
         }
         else{
@@ -25,19 +26,84 @@ public class CladeSpecificAvgGDPairs {
                 br = new BufferedReader( new FileReader( fileLocation ) );
                 ArrayList<String[]> buffer = new ArrayList<>();
                 HashSet<String> cladeValues  = new HashSet<>();
-                String[] temp = null;
+                String[] line;
+                String temp; //To carry the clade values
+                boolean isHeader = true;
                 while ((sCurrentLine = br.readLine()) != null) {
-                    temp = sCurrentLine.trim().split( "\t" );
-                    if(!cladeValues.contains( temp[0] ))
-                        cladeValues.add( temp[0] );
-                    buffer.add( temp );
+                    line = sCurrentLine.trim().split( "\t" );
+                    if(isHeader){
+                         isHeader = false;
+                         //Removing the header info initially itself
+                        continue;
+                    }
+                    temp = line[0].split( "\\." )[0];
+                    if(!cladeValues.contains(temp))
+                        cladeValues.add(temp);
+                    temp = line[1].split( "\\." )[0];
+                    if(!cladeValues.contains(temp))
+                        cladeValues.add(temp);
+                    buffer.add( line );
                 }
 
-                //Removing headers
-                buffer.remove(0);
+
+                ArrayList<CladeSpecific> clades = new ArrayList<>(  );
+                for(String cladeVal: cladeValues){
+                    clades.add(new CladeSpecific( cladeVal ));
+                }
+
+                String[] seq1 = null, seq2 = null;
+                for(String[] data: buffer) {
+                    if (data.length != 3) {
+                        System.out.println( "Wrongly formatted input file" );
+                        System.exit( 0 );
+                    }
+                    seq1 = data[0].split( "\\." );
+                    seq2 = data[1].split( "\\." );
+                    if(seq1.length < 3 && seq2.length < 3 ){
+                        System.out.println("Inaccurate data split");
+                        System.out.println(data);
+                        System.exit( 0 );
+                    }
+
+                    if(!seq1[0].equals( seq2[0] ))
+                        continue;
+                    CladeSpecific clade = CladeSpecific.getClade(clades, seq1[0]);
+                    if(clade == null){
+                        System.out.println("Clades are not loaded in the program properly. Debug it");
+                        System.exit(0);
+                    }
+                   Pair newPair = new Pair( seq1[2] + "." + seq1[3], seq2[2] + "." + seq2[3] );
+                    //removing the same env sample results as they are not necessary
+                    if(newPair.env1.equals( newPair.env2 ))
+                        continue;
+                    int index = Pair.getIndexOfPair( clade.envs, newPair );
+                    if (index != -1) {
+                        newPair = clade.envs.get( index );
+                        newPair.distances.add( Double.parseDouble( data[2] ) );
+                    } else {
+                        newPair.distances.add( Double.parseDouble( data[2] ) );
+                        clade.envs.add( newPair );
+                    }
+                }
+                String outputLocation = args[1].replace("\\","\\\\");
+
+                //Calculating the average
+                for(CladeSpecific clade: clades){
+                    PrintWriter writer = new PrintWriter( outputLocation+"_"+clade.name+".txt");
+                    writer.append("Env1\tEnv2\tAverageGD\n");
+                    for(Pair pair: clade.envs){
+                        pair.calculateMean();
+                        writer.append(pair.env1+"\t"+pair.env2+"\t"+pair.average+"\n");
+                    }
+                    writer.close();
+                }
+                System.out.println("Computations accomplished without any errors!");
+
+
+
 
             } catch (Exception e) {
-                System.out.println( "Input file not found" );
+                //System.out.println( "Input file not found" );
                 e.printStackTrace();
             } finally {
                 try {
@@ -62,11 +128,19 @@ public class CladeSpecificAvgGDPairs {
 
 
 class CladeSpecific{
-    String clade;
+    String name;
     ArrayList<Pair> envs;
     public CladeSpecific(String clade){
-        this.clade = clade;
+        name = clade;
         envs = new ArrayList<>( );
+    }
+
+    public static CladeSpecific getClade(ArrayList<CladeSpecific> cladeDetails, String getClade ){
+        for(CladeSpecific clade: cladeDetails){
+            if(clade.name.equals( getClade ))
+                return clade;
+        }
+        return null;
     }
 
 }
